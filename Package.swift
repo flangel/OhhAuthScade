6f6baaf194a4aec409884535c2d3d92b2f5b778a
Frank Langel <frank@frankjlangel.com>
2019-07-14 18:34:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "OhhAuth",
+    name: "OhhAuthScade",
     products: [
-        .library(name: "OhhAuth", targets: ["OhhAuth"])
+        .library(name: "OhhAuthScade", targets: ["OhhAuthScade"])
     ],
     targets: [
-        .target(name: "OhhAuth", path: ".")
+        .target(name: "OhhAuthScade", path: ".")
     ]
 )
